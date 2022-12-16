5a8d28d0b8251865432e731aacb362b649fd3814
Eetu Haanpää <eehaan@utu.fi>
2022-12-16 20:28:47 +0200

    Token + stuff

--- src/main.py
+++ src/main.py
@@ -1,6 +1,14 @@
 import discord
 import os
 import random
+import json
 
-#Initialize client
 client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Sisäänkirjautuminen käyttäjänä {0.user}'.format(client))
+
+
+
+client.run("MTA1MzM2NDI3MDAyMjI4MzM0NA.Gg9k93.ITZqKkxzBPImBqhMydJxxuG4Qi1fZSPIlAxFTw")
